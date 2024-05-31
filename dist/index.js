0c5db3956c09e05c730a4dcecefd4feeb1323a88
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-05-31 13:11:50 +0300

    insert and delete

--- dist/index.js
+++ dist/index.js
@@ -64,7 +64,7 @@ const server = http_1.default.createServer((req, res) => {
                 let rows = "";
                 result.forEach((s) => {
                     rows += "<tr>";
-                    rows += `<td>${s.name}</td> <td>${s.surname}</td> <td>${s.phone}</td> <td> <a href='/student/${s.id}' class="btn btn-success">Plačiau</a></td>`;
+                    rows += `<td>${s.name}</td> <td>${s.surname}</td> <td>${s.phone}</td> <td> <a href='/student/${s.id}' class="btn btn-success">Plačiau</a><a href='/delete/${s.id}' class="btn btn-danger">Ištrinti</a></td>`;
                     rows += "</tr>";
                 });
                 let template = fs_1.default.readFileSync('templates/students.html').toString();
@@ -72,6 +72,41 @@ const server = http_1.default.createServer((req, res) => {
                 res.write(template);
                 res.end();
             });
+        }
+    }
+    if (url === '/add' && method === 'POST') {
+        if (connected) {
+            const reqBody = [];
+            req.on('data', (d) => {
+                reqBody.push(d);
+            });
+            req.on('end', () => {
+                const reqData = decodeURIComponent(Buffer.concat(reqBody).toString());
+                const dd = reqData.split('&');
+                console.log(dd);
+                const name = mysql2_1.default.escape(dd[0].split('=')[1]);
+                const surname = mysql2_1.default.escape(dd[1].split('=')[1]);
+                const phone = mysql2_1.default.escape(dd[2].split('=')[1]);
+                const sex = mysql2_1.default.escape(dd[3].split('=')[1]);
+                const birthday = mysql2_1.default.escape(dd[4].split('=')[1]);
+                const email = mysql2_1.default.escape(dd[5].split('=')[1]);
+                const sql = `INSERT INTO students(name, surname, phone, sex, birthday, email) VALUES (${name}, ${surname}, ${phone}, ${sex}, ${birthday}, ${email})`;
+                con.query(sql, (error) => {
+                    if (error)
+                        throw error;
+                });
+                res.writeHead(302, {
+                    'location': '/students',
+                });
+                res.end();
+            });
+        }
+    }
+    if (url === '/add' && method === 'GET') {
+        if (connected) {
+            let template = fs_1.default.readFileSync('templates/add.html').toString();
+            res.write(template);
+            res.end();
         }
     }
     if ((url === null || url === void 0 ? void 0 : url.split('/')[1]) === 'student') {
@@ -89,6 +124,17 @@ const server = http_1.default.createServer((req, res) => {
             template = template.replace("{{ birthday }}", student.birthday != null ? student.birthday.toLocaleDateString('lt-LT') : '-');
             template = template.replace("{{ email }}", student.email != null ? student.email : '-');
             res.write(template);
+            res.end();
+        });
+    }
+    if ((url === null || url === void 0 ? void 0 : url.split('/')[1]) === 'delete') {
+        let id = parseInt(url === null || url === void 0 ? void 0 : url.split('/')[2]);
+        con.query(`DELETE FROM students WHERE id=${id}`, (error, result) => {
+            if (error)
+                throw error;
+            res.writeHead(302, {
+                'location': '/students',
+            });
             res.end();
         });
     }
--- src/index.ts
+++ src/index.ts
@@ -71,14 +71,50 @@ const server = http.createServer((req, res) => {
                 let rows = "";
                 result.forEach((s) => {
                     rows+="<tr>";
-                    rows+=`<td>${s.name}</td> <td>${s.surname}</td> <td>${s.phone}</td> <td> <a href='/student/${s.id}' class="btn btn-success">Plačiau</a></td>`;
+                    rows+=`<td>${s.name}</td> <td>${s.surname}</td> <td>${s.phone}</td> <td> <a href='/student/${s.id}' class="btn btn-success">Plačiau</a><a href='/delete/${s.id}' class="btn btn-danger">Ištrinti</a></td>`;
                     rows+="</tr>";
                 });
                 let template = fs.readFileSync('templates/students.html').toString();
-                template = template.replace('{{ students_table }}', rows)
+                template = template.replace('{{ students_table }}', rows);
                 res.write(template);
                 res.end();
             });
+        }
+    }
+
+    if(url === '/add' && method === 'POST') {
+        if(connected){
+            const reqBody: any[] = [];
+            req.on('data', (d) => {
+                reqBody.push(d);
+            });
+            req.on('end', () => {
+                const reqData = decodeURIComponent(Buffer.concat(reqBody).toString());
+                const dd = reqData.split('&');
+                console.log(dd);
+                const name = mysql.escape(dd[0].split('=')[1]);
+                const surname = mysql.escape(dd[1].split('=')[1]);
+                const phone = mysql.escape(dd[2].split('=')[1]);
+                const sex = mysql.escape(dd[3].split('=')[1]);
+                const birthday = mysql.escape(dd[4].split('=')[1]);
+                const email = mysql.escape(dd[5].split('=')[1]);
+                const sql = `INSERT INTO students(name, surname, phone, sex, birthday, email) VALUES (${name}, ${surname}, ${phone}, ${sex}, ${birthday}, ${email})`;
+                con.query(sql, (error) => {
+                    if(error) throw error;
+                });
+                res.writeHead(302, {
+                    'location': '/students',
+                });
+                res.end();
+            })
+        }
+    }
+
+    if(url === '/add' && method === 'GET') {
+        if(connected){
+            let template = fs.readFileSync('templates/add.html').toString();
+            res.write(template);
+            res.end();
         }
     }
 
@@ -98,6 +134,17 @@ const server = http.createServer((req, res) => {
                 template = template.replace("{{ email }}", student.email != null ? student.email : '-');
         
                 res.write(template);
+                res.end();
+            });
+    }
+
+    if(url?.split('/')[1] === 'delete') {
+        let id = parseInt(url?.split('/')[2]);
+        con.query<Student[]>(`DELETE FROM students WHERE id=${id}`, (error, result) => {
+                if(error) throw error; 
+                res.writeHead(302, {
+                    'location': '/students',
+                });
                 res.end();
             });
     }
